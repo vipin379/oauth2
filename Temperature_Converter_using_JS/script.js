ade41a7d272729d1004561f4ad54e09740d6caf7
Kashish <kshishgarg2005@gmail.com>
2025-10-30 01:56:45 +0530

    Added styling to Pasword Generator and necessary validations to Temperature Converter

--- Temperature_Converter_using_JS/script.js
+++ Temperature_Converter_using_JS/script.js
@@ -1,7 +1,15 @@
 function convertTemperature() {
     const temperature = parseFloat(document.getElementById('temperature').value);
     const unit = document.getElementById('unit').value;
+        const alertBox = document.getElementById('alert');
+
     let result;
+    //  Correct input validation
+    if (isNaN(temperature)) {
+        alertBox.innerText = "Please enter a temperature value!";
+        alertBox.style.color = "red";
+        return; // Stop further execution
+    }
     if (unit === 'celsius') {
         resultF = (temperature * 9 / 5) + 32; // Celsius to Fahrenheit
         resultK = temperature + 273.15; // Celsius to Kelvin
@@ -15,6 +23,9 @@ function convertTemperature() {
         resultF = (resultC * 9 / 5) + 32; // Celsius to Fahrenheit
         document.getElementById('result').innerText = `${temperature}K is ${resultC.toFixed(2)}°C and ${resultF.toFixed(2)}°F`;
     }
-
-
 }
+//  Event listener to clear alert when typing starts
+document.getElementById('temperature').addEventListener('input', function() {
+    document.getElementById('alert').innerText = "";
+    document.getElementById('result').innerText = "";
+});
